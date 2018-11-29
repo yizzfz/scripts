4fe60f550694a4b9924c3c109322f7a54136cdee
Han <hc13414@bristol.ac.uk>
2018-11-29 15:06:01 +0000

    get-llvm script

--- get-llvm.sh
+++ get-llvm.sh
@@ -3,13 +3,13 @@ tar -xf llvm-4.0.0.src.tar.xz
 cd llvm-4.0.0.src
 
 cd tools
-wget http://releases.llvm.org/4.0.0/cfe-4.0.0.src.tar.xz -O clang.tar.xz
-tar -xf clang.tar.xz
+wget http://releases.llvm.org/4.0.0/cfe-4.0.0.src.tar.xz
+tar -xf cfe-4.0.0.src.tar.xz && mv cfe-4.0.0.src clang
 cd ..
 
 cd projects
-wget http://releases.llvm.org/4.0.0/compiler-rt-4.0.0.src.tar.xz -O compiler-rt.tar.xz
-tar -xf ccompiler-rt.tar.xz
-wget http://releases.llvm.org/4.0.0/openmp-4.0.0.src.tar.xz -O openmp.tar.xz
-tar -xf openmp.tar.xz
+wget http://releases.llvm.org/4.0.0/compiler-rt-4.0.0.src.tar.xz
+tar -xf compiler-rt-4.0.0.src.tar.xz && mv compiler-rt-4.0.0.src compiler-rt
+wget http://releases.llvm.org/4.0.0/openmp-4.0.0.src.tar.xz
+tar -xf openmp-4.0.0.src.tar.xz && mv openmp-4.0.0.src openmp
 cd ..
